98bb83dbe641f94881d01e07f76729928174f4f2
L-Lawliet-0 <zhang.7199@osu.edu>
2022-03-30 17:32:04 +0800

    3.30

---
+++ Toca/Assets/Scripts/EditorTool/SceneObjectsPlacer.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[ExecuteInEditMode]
+public class SceneObjectsPlacer : MonoBehaviour
+{
+    public bool GENERATEOBJECTS;
+    public bool CLEARALL;
+    public bool INDEXFIX;
+    public bool MOVEINDEX;
+    public Transform Template;
+
+    void Update()
+    {
+
+        if (GENERATEOBJECTS && transform.childCount == 0)
+        {
+            string path = Application.dataPath + "/Resources/RoomOfPrincess";
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] info = dir.GetFiles("*.*");
+            foreach (FileInfo f in info)
+            {
+                // generate objects here
+                if (!f.Name.Contains("meta")) // ignore meta file
+                {
+                    // trim name
+                    string name = f.Name.Split('.')[0];
+                    GameObject obj = new GameObject();
+                    obj.name = name;
+                    obj.transform.SetParent(transform);
+                    obj.AddComponent<SpriteRenderer>();
+                    obj.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("RoomOfPrincess/" + name);
+
+                    PlaceObject(obj.transform);
+                }
+
+            }
+            GENERATEOBJECTS = false;
+        }
+        else if (CLEARALL && transform.childCount > 0)
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+                DestroyImmediate(transform.GetChild(i).gameObject);
+            CLEARALL = false;
+        }
+        else if (INDEXFIX && transform.childCount > 0)
+        {
+            List<Transform> pendingFixes = new List<Transform>();
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (transform.GetChild(i).position.Equals(Vector3.zero))
+                    pendingFixes.Add(transform.GetChild(i));
+            }
+
+            for (int i = 0; i < pendingFixes.Count; i++)
+            {
+                pendingFixes[i].SetSiblingIndex(i);
+            }
+            INDEXFIX = false;
+        }
+        else if (MOVEINDEX && transform.childCount > 0)
+        {
+            List<Transform> pendingFixes = new List<Transform>();
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (transform.GetChild(i).name.Contains("m"))
+                    pendingFixes.Add(transform.GetChild(i));
+            }
+
+            for (int i = 0; i < pendingFixes.Count; i++)
+            {
+                pendingFixes[i].SetSiblingIndex(i);
+            }
+            MOVEINDEX = false;
+        }
+    }
+
+    private void PlaceObject(Transform tran)
+    {
+        string convertedName = tran.name.Replace('-', ' ');
+        for (int i = 0; i < Template.childCount; i++)
+        {
+            if (Template.GetChild(i).name.Equals(convertedName))
+            {
+                tran.position = Template.GetChild(i).position;
+                break;
+            }
+        }
+    }
+}
--- Toca/Assets/Scripts/InputManager.cs
+++ Toca/Assets/Scripts/InputManager.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public bool TouchMode;
+
     private static InputManager m_Instance;
     public static InputManager Instance { get { return m_Instance; } }
     public bool InSelection; // the input is currently selecting something
     public TouchControl SelectedObject; // the object this input is selected
 
+    private Dictionary<Touch, TouchInfo> Touches;
 
-    private void Awake()
+    private class TouchInfo
     {
-        m_Instance = this;
-        InSelection = false;
-    }
+        private TouchControl SelectedObject;
+        public bool InSelection;
+        public Touch MyTouch;
 
-    // The job of the input manager is to detect input and fire events
-
-    private void Update()
-    {
-        if (!InSelection)
+        public TouchInfo(Touch touch)
         {
-            Vector3 pos = GlobalParameter.Instance.ScreenPosToGamePos(Input.mousePosition);
+            MyTouch = touch;
+            InSelection = false;
+            SelectedObject = null;
+        }
+
+        public void FrameUpdate()
+        {
+            if (MyTouch.phase == TouchPhase.Began)
+                OnTouch();
+            else if (MyTouch.phase == TouchPhase.Canceled || MyTouch.phase == TouchPhase.Ended)
+                Detouch();
+            else
+                UpdatePos();
+        }
+
+        public void OnTouch()
+        {
+            Vector3 pos = GlobalParameter.Instance.ScreenPosToGamePos(MyTouch.position);
 
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 999, 1 << LayerMask.NameToLayer("Selection"));
             if (hit)
@@ -32,37 +48,131 @@ public class InputManager : MonoBehaviour
             else
                 SelectedObject = null;
 
-            /*
-            Collider2D collider = Physics2D.OverlapPoint(pos, 1 << LayerMask.NameToLayer("Selection"));
-            if (collider)
-            {
-                SelectedObject = collider.GetComponentInParent<TouchControl>();
-                if (!SelectedObject)
-                    SelectedObject = collider.GetComponent<TouchControl>();
-            }
-            else
-                SelectedObject = null;
-            */
-
-            if (Input.GetMouseButtonDown(0) && SelectedObject)
+            if (SelectedObject)
             {
                 // finger touched object
                 SelectedObject.OnTouch(pos);
                 InSelection = true;
             }
         }
+
+        public void Detouch()
+        {
+            if (InSelection)
+            {
+                InSelection = false;
+                SelectedObject.OnDeTouch();
+            }
+        }
+
+        public void UpdatePos()
+        {
+            if (InSelection)
+            {
+                SelectedObject.OnTouchPositionChanged(GlobalParameter.Instance.ScreenPosToGamePos(MyTouch.position));
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        m_Instance = this;
+        InSelection = false;
+
+        Touches = new Dictionary<Touch, TouchInfo>();
+    }
+
+    // The job of the input manager is to detect input and fire events
+
+    private void Update()
+    {
+        if (!TouchMode)
+        {
+            if (!InSelection)
+            {
+                SelectedObject = null;
+                Vector3 pos = GlobalParameter.Instance.ScreenPosToGamePos(Input.mousePosition);
+                pos.z = 0;
+
+                RaycastHit2D[] hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 999, 1 << LayerMask.NameToLayer("Selection"));
+                float maxLayer = float.MinValue;
+                foreach (RaycastHit2D hit in hits)
+                {
+                    TouchControl tc = hit.collider.GetComponentInParent<TouchControl>();
+                    LayerControl lc = (LayerControl)tc.TocaObject.GetTocaFunction<LayerControl>();
+                    float layerValue = lc.OrderValue;
+                    if (layerValue > maxLayer)
+                    {
+                        maxLayer = layerValue;
+                        SelectedObject = tc;
+                    }
+                }
+
+                /*
+                if (hit)
+                {
+                    SelectedObject = hit.collider.GetComponentInParent<TouchControl>();
+                }
+                else
+                    SelectedObject = null;
+
+                /*
+                Collider2D collider = Physics2D.OverlapPoint(pos, 1 << LayerMask.NameToLayer("Selection"));
+                if (collider)
+                {
+                    SelectedObject = collider.GetComponentInParent<TouchControl>();
+                    if (!SelectedObject)
+                        SelectedObject = collider.GetComponent<TouchControl>();
+                }
+                else
+                    SelectedObject = null;
+                */
+
+                if (Input.GetMouseButtonDown(0) && SelectedObject)
+                {
+                    // finger touched object
+                    SelectedObject.OnTouch(pos);
+                    InSelection = true;
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonUp(0))
+                {
+                    // finger lifted up
+                    SelectedObject.OnDeTouch();
+                    InSelection = false;
+                }
+                else if (Input.GetMouseButton(0))
+                {
+                    // finger position changed
+                    SelectedObject.OnTouchPositionChanged(GlobalParameter.Instance.ScreenPosToGamePos(Input.mousePosition));
+                }
+            }
+        }
         else
         {
-            if (Input.GetMouseButtonUp(0))
+            Touch[] touches = Input.touches;
+            foreach (Touch touch in touches)
             {
-                // finger lifted up
-                SelectedObject.OnDeTouch();
-                InSelection = false;
+                if (!Touches.ContainsKey(touch))
+                {
+                    TouchInfo info = new TouchInfo(touch);
+                    Touches.Add(touch, info);
+                }
             }
-            else if (Input.GetMouseButton(0))
+
+            List<Touch> removeThisFrame = new List<Touch>();
+            foreach (Touch touch in touches)
             {
-                // finger position changed
-                SelectedObject.OnTouchPositionChanged(GlobalParameter.Instance.ScreenPosToGamePos(Input.mousePosition));
+                Touches[touch].FrameUpdate();
+                if (touch.phase == TouchPhase.Canceled || touch.phase == TouchPhase.Ended)
+                    removeThisFrame.Add(touch);
+            }
+
+            foreach (Touch touch in removeThisFrame)
+            {
+                Touches.Remove(touch);
             }
         }
     }
